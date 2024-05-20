3f8548605a81c248fb8bcdc2b6499e313bbf3e54
Esther White <monacodelisa@gmail.com>
2024-05-20 14:07:04 +0300

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { RouterLink, RouterOutlet } from "@angular/router";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatButtonModule } from "@angular/material/button";
+import { ThemeService } from "./services/theme.service";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [
+		RouterOutlet,
+    RouterLink,
+		MatCardModule,
+		MatToolbarModule,
+		MatSlideToggleModule,
+		MatButtonModule,
+	],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'techshwocase';
+	title = "TechShwocase";
+  themeService = inject(ThemeService);
+
+  toggleTheme() {
+    this.themeService.updateTheme();
+  }
 }
